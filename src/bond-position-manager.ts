20478d48f109c8e381347fb9372a4d511930b3de
graykode <nlkey2022@gmail.com>
2023-08-22 22:48:22 +0900

    fix: fix to token:substitute -> 1:N

--- src/bond-position-manager.ts
+++ src/bond-position-manager.ts
@@ -1,13 +1,13 @@
-import { AssetRegistered } from '../generated/BondPositionManager/BondPositionManager'
+import { RegisterAsset } from '../generated/BondPositionManager/BondPositionManager'
 import { Substitute as AssetContract } from '../generated/BondPositionManager/Substitute'
-import { Asset, Token } from '../generated/schema'
+import { Substitute, Token } from '../generated/schema'
 
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from './helpers'
 
-export function handleAssetRegistered(event: AssetRegistered): void {
-  const assetAddress = event.params.asset
-  const assetContract = AssetContract.bind(assetAddress)
-  const underlyingToken = assetContract.underlyingToken()
+export function handleRegisterAsset(event: RegisterAsset): void {
+  const substituteAddress = event.params.asset
+  const substituteContract = AssetContract.bind(substituteAddress)
+  const underlyingToken = substituteContract.underlyingToken()
 
   let token = Token.load(underlyingToken.toHexString())
   if (token === null) {
@@ -15,16 +15,16 @@ export function handleAssetRegistered(event: AssetRegistered): void {
     token.symbol = fetchTokenSymbol(underlyingToken)
     token.name = fetchTokenName(underlyingToken)
     token.decimals = fetchTokenDecimals(underlyingToken)
-    token.asset = assetAddress.toHexString()
     token.save()
   }
 
-  let asset = Asset.load(assetAddress.toHexString())
-  if (asset === null) {
-    asset = new Asset(assetAddress.toHexString())
-    asset.symbol = fetchTokenSymbol(assetAddress)
-    asset.name = fetchTokenName(assetAddress)
-    asset.decimals = fetchTokenDecimals(assetAddress)
-    asset.save()
+  let substitute = Substitute.load(substituteAddress.toHexString())
+  if (substitute === null) {
+    substitute = new Substitute(substituteAddress.toHexString())
+    substitute.symbol = fetchTokenSymbol(substituteAddress)
+    substitute.name = fetchTokenName(substituteAddress)
+    substitute.decimals = fetchTokenDecimals(substituteAddress)
+    substitute.underlying = underlyingToken.toHexString()
+    substitute.save()
   }
 }
