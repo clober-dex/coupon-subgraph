e62bc856ec0467bceeec3d1da8bcbe83213c9622
graykode <nlkey2022@gmail.com>
2023-08-30 18:38:47 +0900

    feat: add `principal`

--- src/bond-position-manager.ts
+++ src/bond-position-manager.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, store } from '@graphprotocol/graph-ts'
+import { Address, BigInt, store, ethereum } from '@graphprotocol/graph-ts'
 
 import {
   BondPositionManager as BondPositionManagerContract,
@@ -6,6 +6,7 @@ import {
   Transfer,
   UpdatePosition,
 } from '../generated/BondPositionManager/BondPositionManager'
+import { OrderBook as OrderBookContract } from '../generated/templates/OrderNFT/OrderBook'
 import { Substitute as AssetContract } from '../generated/BondPositionManager/Substitute'
 import { BondPosition } from '../generated/schema'
 
@@ -36,15 +37,45 @@ export function handleUpdateBondPosition(event: UpdatePosition): void {
   const bondPositionManager = BondPositionManagerContract.bind(
     Address.fromString(BOND_POSITION_MANAGER_ADDRESS),
   )
+
+  const takeEvents = (event.receipt as ethereum.TransactionReceipt).logs.filter(
+    (log) =>
+      log.topics[0].toHexString() ==
+      '0x9754cddf091a07317cd2bfc6ead4610d1a7401cf0ea4176a44ab9366d4042d7d',
+  )
+
+  let boughtAmount = BigInt.zero()
+  let soldAmount = BigInt.zero()
+  for (let i = 0; i < takeEvents.length; i++) {
+    const orderBookContract = OrderBookContract.bind(takeEvents[i].address)
+    const decoded = ethereum.decode(
+      '(uint16,uint64,uint8)',
+      takeEvents[i].data,
+    ) as ethereum.Value
+    const data = decoded.toTuple()
+    const rawAmount = data[1].toBigInt()
+    const options = data[2].toI32()
+    if (options & 0x1) {
+      // bid
+      boughtAmount = boughtAmount.plus(orderBookContract.rawToQuote(rawAmount))
+    } else {
+      // ask
+      soldAmount = soldAmount.plus(orderBookContract.rawToQuote(rawAmount))
+    }
+  }
+
   let bondPosition = BondPosition.load(tokenId.toString())
   if (bondPosition === null) {
     bondPosition = new BondPosition(tokenId.toString())
   }
+  bondPosition.user = bondPositionManager.ownerOf(tokenId).toHexString()
   const position = bondPositionManager.getPosition(tokenId)
   const currentEpochIndex = getEpochIndexByTimestamp(event.block.timestamp)
 
-  bondPosition.user = bondPositionManager.ownerOf(tokenId).toHexString()
-  bondPosition.amount = position.amount
+  bondPosition.amount = event.params.amount
+  bondPosition.principal = event.params.amount
+    .minus(soldAmount)
+    .plus(boughtAmount)
   bondPosition.startEpoch = currentEpochIndex
   bondPosition.startTimestamp = getStartTimestamp(currentEpochIndex)
   bondPosition.expiryEpoch = BigInt.fromI32(position.expiredWith)
