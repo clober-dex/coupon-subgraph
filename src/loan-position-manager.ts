635a6892019667bc5cc9e8d9aeced0991dcd2227
graykode <nlkey2022@gmail.com>
2023-09-04 19:47:34 +0900

    feat: loan position

--- src/loan-position-manager.ts
+++ src/loan-position-manager.ts
@@ -1,13 +1,20 @@
-import { Address } from '@graphprotocol/graph-ts'
+import { Address, BigInt, ethereum, store, log } from '@graphprotocol/graph-ts'
 
 import {
   SetLoanConfiguration,
   LoanPositionManager as LoanPositionManagerContract,
+  UpdatePosition,
 } from '../generated/LoanPositionManager/LoanPositionManager'
 import { Substitute as AssetContract } from '../generated/LoanPositionManager/Substitute'
-import { Collateral } from '../generated/schema'
+import { Collateral, LoanPosition } from '../generated/schema'
+import { OrderBook as OrderBookContract } from '../generated/templates/OrderNFT/OrderBook'
 
-import { createAsset, createToken } from './helpers'
+import {
+  createAsset,
+  createEpoch,
+  createToken,
+  getEpochIndexByTimestamp,
+} from './helpers'
 
 export function handleSetLoanConfiguration(event: SetLoanConfiguration): void {
   createToken(event.params.collateral)
@@ -40,4 +47,74 @@ export function handleSetLoanConfiguration(event: SetLoanConfiguration): void {
   const asset = createAsset(Address.fromString(substituteUnderlying.id))
   asset.collaterals = asset.collaterals.concat([key])
   asset.save()
+}
+
+export function handleUpdateLoanPosition(event: UpdatePosition): void {
+  const takeEvents = (event.receipt as ethereum.TransactionReceipt).logs.filter(
+    (log) =>
+      log.topics[0].toHexString() ==
+      '0x9754cddf091a07317cd2bfc6ead4610d1a7401cf0ea4176a44ab9366d4042d7d',
+  )
+  let boughtAmount = BigInt.zero()
+  let soldAmount = BigInt.zero()
+  for (let i = 0; i < takeEvents.length; i++) {
+    const orderBookContract = OrderBookContract.bind(takeEvents[i].address)
+    const decoded = ethereum.decode(
+      '(uint16,uint64,uint8)',
+      takeEvents[i].data,
+    ) as ethereum.Value
+    const data = decoded.toTuple()
+    const rawAmount = data[1].toBigInt()
+    const options = data[2].toI32()
+    if (options & 0x1) {
+      // bid (withdraw)
+      boughtAmount = boughtAmount.plus(orderBookContract.rawToQuote(rawAmount))
+    } else {
+      // ask (deposit)
+      soldAmount = soldAmount.plus(orderBookContract.rawToQuote(rawAmount))
+    }
+  }
+
+  const positionId = event.params.positionId
+  const loanPositionManager = LoanPositionManagerContract.bind(event.address)
+  const position = loanPositionManager.getPosition(positionId)
+
+  let loanPosition = LoanPosition.load(positionId.toString())
+  if (loanPosition === null) {
+    loanPosition = new LoanPosition(positionId.toString())
+    loanPosition.amount = BigInt.zero()
+    loanPosition.principal = BigInt.zero()
+  }
+  // const previousAmount = loanPosition.amount
+  let mightBeDeleted = false
+  if (event.params.debtAmount.equals(BigInt.zero())) {
+    mightBeDeleted = true
+    log.info('mightBeDeleted', [positionId.toString()])
+  } else {
+    loanPosition.user = loanPositionManager.ownerOf(positionId).toHexString()
+    loanPosition.collateral = position.collateralToken
+      .toHexString()
+      .concat('-')
+      .concat(position.debtToken.toHexString())
+    loanPosition.collateralAmount = position.collateralAmount
+    loanPosition.principal = loanPosition.principal
+      .plus(event.params.debtAmount)
+      .minus(loanPosition.amount)
+      .plus(soldAmount)
+      .minus(boughtAmount)
+    loanPosition.amount = event.params.debtAmount
+    loanPosition.fromEpoch = createEpoch(
+      getEpochIndexByTimestamp(event.block.timestamp),
+    ).id
+    loanPosition.toEpoch = createEpoch(BigInt.fromI32(position.expiredWith)).id
+    loanPosition.substitute = position.debtToken.toHexString()
+    loanPosition.underlying = AssetContract.bind(position.debtToken)
+      .underlyingToken()
+      .toHexString()
+    loanPosition.save()
+  }
+
+  if (mightBeDeleted) {
+    store.remove('LoanPosition', positionId.toString())
+  }
 }
